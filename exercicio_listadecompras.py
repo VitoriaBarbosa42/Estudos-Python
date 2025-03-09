8642854f5a7faf665c42b0c47c1d6d0079316322
Estudos-Python <https://github.com/VitoriaBarbosa42/Estudos-Python.git>
2025-03-09 17:42:11 -0300

    Algumas melhorias para deixar o codigo mais limpo - sugestão do chat

--- exercicio_listadecompras.py
+++ exercicio_listadecompras.py
@@ -1,60 +1,61 @@
 botao = None
 lista_de_compras = []
 ind = 0
-item_apagar = True
 
 while True:
 
     print ('Escolha uma opção ')
     botao = input ('[i]nsetir [a]pagar [l]istar [t]rocar: ').lower()
-    item_apagar = True
 
     if botao == 'i':
 
-        while item_apagar:
-            item = input(f'Qual item gostaria de adicionar? Digite [s] se deseja voltar ao menu ')
-            item.lower
-            sair = item == 's'
-            if sair == False and len(item) >= 1:
-                print (f'Item {item} adicionado com sucesso')
-                lista_de_compras.append(item)
-                
-            if sair:
-                item_apagar = False
+        while True:
+            adicionar_item = input(f'Qual item gostaria de adicionar? Digite [s] se deseja voltar ao menu ').lower().strip()
+            if adicionar_item == 's':
+                break
+            if len(adicionar_item) >= 1:
+                lista_de_compras.append(adicionar_item)
+                print (f'Item {adicionar_item} adicionado com sucesso')
+            else:
+                print('Digite um item válido')
     elif botao == 'a':
             
-        while item_apagar:
+        while True:
             try:
                 for i, item in enumerate(lista_de_compras):
                         print(i, item)
                         
-                i = int (input('Qual item gostaria de apagar? '))
-                ultimo_valor = lista_de_compras.pop(i)
+                ind = int (input('Qual item gostaria de apagar? '))
+                ultimo_valor = lista_de_compras.pop(ind)
                 print (f'O item {ultimo_valor} foi apagado da sua lista')
-                item_apagar = False
+                break
             except:
                 print ('Indice invalido. Tente novamente')    
         
     elif botao == 't':
-            while item_apagar:
+        while True:
 
-                try:
-                    for i, item in enumerate(lista_de_compras):
-                        print(i, item)
+            if not lista_de_compras:
+                print ('a lista esta vazia')
+                break
 
-                    alterar = int (input ('Qual item da lista deseja alterar: '))
-                    alteracao = input ('qual item deseja acrescentar no lugar: ')
-                    lista_de_compras [alterar] = alteracao
-                    item_apagar = False
-                    print ('Seu item foi ALTERADO com SUCESSO!')
-            
+            try:
+                for i, item in enumerate(lista_de_compras):
+                    print(i, item)
 
-                except:
-                    print ('Item INVALIDO, tente novamente.')
-                    print (lista_de_compras)
-                    sair1 = input ('Caso queira voltar ao menu digite "s" caso contrario digite "n": ').lower
-                    if sair1 == 's':
-                        item_apagar = False
+                alterar = int (input ('Qual item da lista deseja alterar: '))
+                alteracao = input ('qual item deseja acrescentar no lugar: ')
+                lista_de_compras [alterar] = alteracao
+                print ('Seu item foi ALTERADO com SUCESSO!')
+                break
+        
+
+            except:
+                sair1 = input('Item INVÁLIDO. Deseja voltar ao menu? [s/n]: ').lower()
+                if sair1 == 's':
+                    break
+                else:
+                    print('Vamos tentar novamente...')
 
 
     elif botao == 'l':
