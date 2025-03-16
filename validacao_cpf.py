feaf0d0ce826abf0519ec5d64fc64246cbd327c7
Estudos-Python <https://github.com/VitoriaBarbosa42/Estudos-Python.git>
2025-03-16 14:37:02 -0300

    Programa em Python que usa a formula para validar CPF

---
+++ validacao_cpf.py
@@ -0,0 +1,49 @@
+while True:
+    cpf = input ('Digite o CPF que gostaria de validar: ')
+    print (f'O CPF "{cpf}" esta correto?')
+    confirmacao = input ('[s]im - [n]ão --- [sair] se quiser encerrar o programa ').lower()
+    soma_cpf1 = 0
+    soma_cpf2 = 0
+    n_multiplicador1 = 10
+    n_multiplicador2 = 11
+    digitos_int_cpf = []
+
+    if confirmacao == 's':
+        
+        if len(cpf) == 11 and cpf.isdigit():
+            for i in cpf[:9]:
+                digitos_int_cpf.append (int(i))
+
+            for i in digitos_int_cpf:
+                soma_cpf1 += i * n_multiplicador1 
+                n_multiplicador1 -= 1
+            
+            validacao1 = (soma_cpf1 * 10) % 11
+            if validacao1 > 9:
+                validacao1 = 0
+                
+            digitos_int_cpf.append(validacao1)
+
+            for i in digitos_int_cpf:
+                soma_cpf2 += i * n_multiplicador2
+                n_multiplicador2 -= 1
+                
+            validacao2 = (soma_cpf2 * 10) % 11
+            if validacao2 > 9:
+                validacao2 = 0
+            
+            digito1 = int (cpf[9])
+            digito2 = int (cpf[10])
+
+            if validacao1 == digito1 and validacao2 == digito2:
+                print ('o cpf é valido')
+            else:
+                print ('o cpf é invalido')
+
+        else:
+            print ('Digite um valor valido') 
+    elif confirmacao == 'sair':
+        print ('Volte sempre :)')
+        break
+    else:
+        print ('Tente novamente')
