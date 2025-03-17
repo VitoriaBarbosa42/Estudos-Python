4352257747d4c0938b36f727169427d5f00488e8
Estudos-Python <https://github.com/VitoriaBarbosa42/Estudos-Python.git>
2025-03-16 22:16:24 -0300

    Gerando 10 CPF ao mesmo tempo

--- gerador_cpf.py
+++ gerador_cpf.py
@@ -8,40 +8,42 @@ while True:
 
 
     if sair == '1':
-        base_cpf = random.choices(range(10), k=9)
-        digito_mul1 = 10
-        soma_digito1 = 0
+            
+        for _ in range (10):
+            base_cpf = random.choices(range(10), k=9)
+            digito_mul1 = 10
+            soma_digito1 = 0
 
-        for i in base_cpf:
-            soma_digito1 += i * digito_mul1
-            digito_mul1 -= 1
-        digito1 = (soma_digito1*10) % 11
+            for i in base_cpf:
+                soma_digito1 += i * digito_mul1
+                digito_mul1 -= 1
+            digito1 = (soma_digito1*10) % 11
 
-        if digito1 > 9: digito1 = 0
+            if digito1 > 9: digito1 = 0
 
-        base_cpf.append(digito1)
-        digito_mul2 = 11
-        soma_digito2 = 0
+            base_cpf.append(digito1)
+            digito_mul2 = 11
+            soma_digito2 = 0
 
-        for i in base_cpf:
-            soma_digito2 += i * digito_mul2
-            digito_mul2 -= 1
+            for i in base_cpf:
+                soma_digito2 += i * digito_mul2
+                digito_mul2 -= 1
 
-        digito2 = (soma_digito2*10) % 11
-        if digito2 > 9: digito2 = 0
+            digito2 = (soma_digito2*10) % 11
+            if digito2 > 9: digito2 = 0
 
-        base_cpf.append(digito2)
+            base_cpf.append(digito2)
 
-        cpf_str = [str(digito) for digito in base_cpf]
+            cpf_str = [str(digito) for digito in base_cpf]
 
-        cpf_formatado = (
-            ''.join(cpf_str[0:3]) + '.' +
-            ''.join(cpf_str[3:6]) + '.' +
-            ''.join(cpf_str[6:9]) + '-' +
-            ''.join(cpf_str[9:])
-        )
-        print("Novo CPF: ", cpf_formatado)
-        print()
+            cpf_formatado = (
+                ''.join(cpf_str[0:3]) + '.' +
+                ''.join(cpf_str[3:6]) + '.' +
+                ''.join(cpf_str[6:9]) + '-' +
+                ''.join(cpf_str[9:])
+            )
+            print("Novo CPF: ", cpf_formatado)
+            print()
     elif sair == '2':
         print ('Até a próxima :)')
         break
